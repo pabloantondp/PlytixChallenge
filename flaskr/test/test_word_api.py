20913410424ea2999587dfdcd79b1e9ed5de23a5
Pablo Anton <paban@ciklum.com>
2021-11-30 09:53:16 +0100

    Errors: Adding error handlers and word endpoints

--- flaskr/test/test_word_api.py
+++ flaskr/test/test_word_api.py
@@ -9,6 +9,16 @@ def test_api_get_empty(empty_db_client):
     assert rv.data is not None
 
 
+def test_api_malformed_post(empty_db_client):
+    response = empty_db_client.post(
+        '/words',
+        data=json.dumps({"word": "palabra"}),
+        content_type='application/json',
+    )
+
+    assert response.status_code == 404
+
+
 def test_api_count_empty(empty_db_client):
     assert empty_db_client is not None
     response = empty_db_client.get('/words')
@@ -16,6 +26,20 @@ def test_api_count_empty(empty_db_client):
     data = json.loads(response.get_data(as_text=True))
     assert response.status_code == 200
     assert len(data['data']) == 0
+
+
+def test_api_get(initialized_db_client):
+    response = initialized_db_client.get('/words')
+
+    json_data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    data = json_data['data']
+    assert len(data) == 5
+    assert data[0] == "cosa"
+    assert data[1] == "caso"
+    assert data[2] == "paco"
+    assert data[3] == "pepe"
+    assert data[4] == "Malaga"
 
 
 def test_api_post(initialized_db_client):
@@ -32,17 +56,49 @@ def test_api_post(initialized_db_client):
     assert data['word'] == "calle"
     assert data['position'] == 3
 
-
-def test_api_get(initialized_db_client):
     response = initialized_db_client.get('/words')
 
     json_data = json.loads(response.get_data(as_text=True))
     assert response.status_code == 200
     data = json_data['data']
-    assert len(data) == 5
+    assert len(data) == 6
     assert data[0] == "cosa"
     assert data[1] == "caso"
-    assert data[2] == "paco"
-    assert data[3] == "pepe"
-    assert data[4] == "Malaga"
+    assert data[2] == "calle"
+    assert data[3] == "paco"
+    assert data[4] == "pepe"
+    assert data[5] == "Malaga"
 
+
+def test_api_patch(initialized_db_client):
+    test_word = Word(word="calle", position=3)
+    patch_data = {"position": 5}
+    response = initialized_db_client.post(
+        '/words',
+        data=test_word.to_json(),
+        content_type='application/json',
+    )
+    assert response.status_code == 200
+
+    response = initialized_db_client.patch('/words/calle',
+                                          data=json.dumps({"position": 5}),
+                                          content_type='application/json')
+    json_data = json.loads(response.get_data(as_text=True))
+    assert response.status_code == 200
+    assert json_data['word'] == "calle"
+    assert json_data['position'] == 5
+
+
+def test_api_delete_error(empty_db_client):
+    response = empty_db_client.delete('/words/what')
+    assert response.status_code == 404
+
+
+def test_api_delete_error(empty_db_client):
+    response = empty_db_client.delete('/words/what')
+    assert response.status_code == 404
+
+
+def test_api_patch_error(empty_db_client):
+    response = empty_db_client.delete('/words/what')
+    assert response.status_code == 404
--- flaskr/words/view.py
+++ flaskr/words/view.py
@@ -1,24 +1,76 @@
 from flask import Blueprint, request, current_app, jsonify
+from flask_pymongo import ASCENDING, DESCENDING
+from flask_pymongo.wrappers import Collection
+from pydantic import ValidationError
+from pymongo.errors import DuplicateKeyError
+
 import flaskr
 from flaskr.words.model import Word
 
 words_blueprint = Blueprint('words', __name__)
+words_collection: Collection = None
+
+
+@words_blueprint.app_errorhandler(404)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
+
+
+@words_blueprint.app_errorhandler(ValidationError)
+def resource_not_found(e):
+    return jsonify(error=str(e)), 404
+
+
+@words_blueprint.app_errorhandler(AssertionError)
+def resource_not_found(e):
+    return jsonify(error=f"Request body error"), 400
+
+
+@words_blueprint.app_errorhandler(DuplicateKeyError)
+def resource_not_found(e):
+    return jsonify(error=f"Duplicate key error."), 400
+
+
+@words_blueprint.before_app_first_request
+def init_my_blueprint():
+    global words_collection
+    words_collection = flaskr.pymongo.db.words
+    current_app.logger.info('First app request')
+    words_collection.create_index([("name", ASCENDING), ("_id", DESCENDING)], unique=True)
 
 
 @words_blueprint.route('/words', methods=['GET', 'POST'])
-def words():
+def words_path():
 
     if request.method == 'POST':
         raw_word = request.get_json()
         current_app.logger.info(raw_word)
         word = Word(**raw_word)
-        insert_result = flaskr.pymongo.db.words.insert_one(word.to_bson())
 
+        insert_result = words_collection.insert_one(word.to_bson())
         # TODO db error handling
         return word.to_json()
 
     elif request.method == 'GET':
-        items = flaskr.pymongo.db.words.find({}).sort("position", 1)
-        data = [item['word'] for item in list(items)]
-        current_app.logger.info(data)
+        items = words_collection.find({}).sort([("position", ASCENDING), ("_id", DESCENDING)])
+        data = [item['word'] for item in items]
         return jsonify({"data": data})
+
+
+@words_blueprint.route('/words/<word>', methods=['PATCH', 'DELETE'])
+def word_path(word):
+    words_collection.find_one_or_404({"word": word})
+
+    if request.method == 'PATCH':
+        raw_word = request.get_json()
+        assert len(raw_word) == 1
+        raw_word['word'] = word
+        new_word = Word(**raw_word)
+
+        words_collection.delete_one({"word": word})
+        words_collection.insert_one(new_word.to_bson())
+        return new_word.to_json()
+
+    elif request.method == 'DELETE':
+        response = words_collection.delete_one({"word": word})
+        return 204
